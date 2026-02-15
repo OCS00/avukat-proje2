4786a3ee75b72aa3a11f1f79159447301eab31c4
onurcemshn <onurcem154@gmail.com>
2026-02-15 20:45:03 +0300

    Dosya isimleri ve index.js duzeltildi

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,5 +1,10 @@
-import {postType} from './post'
-import {categoryType} from './category'
+// src/sanity/schemaTypes/index.js
+
+// Dosya adın 'post.js' ise './post' yazmalısın
+import { postType } from './post' 
+
+// Dosya adın 'category.js' ise './category' yazmalısın
+import { categoryType } from './category'
 
 export const schema = {
   types: [postType, categoryType],
